29261a681455be428aaffff621483c5f936595bf
Luiz Fernando <luizkilerr@outlook.com>
2022-02-23 20:05:03 -0300

    Answer(How are you today)

--- Projeto-teste/Program.cs
+++ Projeto-teste/Program.cs
@@ -7,8 +7,9 @@ namespace Projeto_teste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            
+            Console.WriteLine("How are you today");
             Console.ReadLine();
+          
         }
     }
 }
